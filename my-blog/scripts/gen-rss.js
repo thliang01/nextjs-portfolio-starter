b7ac8239eda10070301a10925a842317fd652efe
Thomas <thliang01@gmail.com>
2023-03-01 20:25:50 +0800

    feat: update Portfolio

--- my-blog/scripts/gen-rss.js
+++ my-blog/scripts/gen-rss.js
@@ -7,11 +7,11 @@ async function generate() {
   const feed = new RSS({
     title: 'G.T Liang',
     site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    feed_url: 'https://yoursite.com/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
-  const allPosts = []
+
   await Promise.all(
     posts.map(async (name) => {
       if (name.startsWith('index.')) return
@@ -21,21 +21,17 @@ async function generate() {
       )
       const frontmatter = matter(content)
 
-      allPosts.push({
+      feed.item({
         title: frontmatter.data.title,
         url: '/posts/' + name.replace(/\.mdx?/, ''),
         date: frontmatter.data.date,
         description: frontmatter.data.description,
         categories: frontmatter.data.tag.split(', '),
-        author: frontmatter.data.author
+        author: frontmatter.data.author,
       })
     })
   )
 
-  allPosts.sort((a, b) => new Date(b.date) - new Date(a.date))
-  allPosts.forEach((post) => {
-      feed.item(post)
-  })
   await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
 }
 
